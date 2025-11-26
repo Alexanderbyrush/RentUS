7cc8c3eba89ee5c10b8fb680b4d1dd97a914c5c9
miguelcamilok <miguelcamilok@gmail.com>
2025-11-26 14:31:57 -0500

    feat(profile): implement full profile view logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,11 @@ class User extends Authenticatable implements JWTSubject
         'id_documento',          // Documento de identificación del usuario
         'status',                // Estado del usuario (activo, inactivo, etc.)
         'registration_date',     // Fecha de registro del usuario
-        'password'               // Contraseña del usuario (sin hash, se usa para autenticación)
+        'password',                // Contraseña del usuario (sin hash, se usa para autenticación)
+        'photo',
+        'bio',
+        'department',
+        'city'
     ];
 
     // Ocultar campos sensibles en respuestas JSON
@@ -39,17 +43,35 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime'
     ];
 
-    public function properties(){return $this->hasMany(Property::class);}
+    public function properties()
+    {
+        return $this->hasMany(Property::class);
+    }
 
-    public function contracts(){return $this->hasMany(Contract::class);}
+    public function contracts()
+    {
+        return $this->hasMany(Contract::class);
+    }
 
-    public function reports(){return $this->hasMany(Report::class);}
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
 
-    public function maintenances(){return $this->hasMany(Maintenance::class);}
+    public function maintenances()
+    {
+        return $this->hasMany(Maintenance::class);
+    }
 
-    public function rentalRequest(){return $this->hasMany(RentalRequest::class);}
+    public function rentalRequest()
+    {
+        return $this->hasMany(RentalRequest::class);
+    }
 
-    public function ratings(){return $this->hasMany(Rating::class);}
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -78,7 +100,7 @@ class User extends Authenticatable implements JWTSubject
         ];
     }
 
-        public function getJWTIdentifier()
+    public function getJWTIdentifier()
     {
         return $this->getKey(); // normalmente es el id
     }
